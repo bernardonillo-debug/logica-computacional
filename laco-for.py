2aa4d8d25f7b775da69f98548dd16a82df9cc151
bernardonillo-debug <bernardo.nillo@escola.pr.gov.br>
2025-09-22 12:15:48 -0300

    Create laco-for.py

---
+++ laco-for.py
@@ -0,0 +1,30 @@
+for i in range(1, 12, 2):
+
+    print(i)
+
+for i in range(1, 9):
+
+    print(i)
+
+for i in range(3):
+
+    print("Estudando lógica!")
+
+for i in range(10, 0, -1):
+
+    print(i)
+
+for i in range(5):
+    print("Python é lógico!")
+
+frase = 'tenha um dia feliz'
+repeticao = 5
+
+for i in range(repeticao):
+
+    print(frase)
+
+for i in range(3, 31, 6):
+    print(i)
+
+
